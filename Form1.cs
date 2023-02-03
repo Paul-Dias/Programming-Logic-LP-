f79978ecc2101e989b5d1f2e3c80d873c64bdc6a
-P <84252242+Paul-Dias@users.noreply.github.com>
2023-02-03 12:43:25 -0300

    Add files via upload
    Somador em C#

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int t = 0;
+
+            for (int n = 0; n <= 100; n++)
+            {
+                t = t + n;
+            }
+            MessageBox.Show("A soma dos 100 primeiros numeros vale: " + t);
+        }
+    }
+}
